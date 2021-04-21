e3b1ab3e4a30d57d44efbe769bf416b5d7816e6f
Ingo <foren@asche-rz.de>
2021-04-21 21:16:43 +0200

    Changed detection of running backup

--- nas_ab_status_m.sh
+++ nas_ab_status_m.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Version 1.0.3
+# Version 1.0.4
 
 CONF=$1
 source $CONF
@@ -9,21 +9,25 @@ echo "<?xml version=\"10.0\" encoding=\"UTF-8\" ?><prtg>"
 for DEVICE in "${DEVICES[@]}"
 do
 IFS="|" read RESULTID TASKID <<< `sqlite3 /volume1/@ActiveBackup/activity.db "select result_id,task_id from result_table where task_name like '$TASK' AND task_config like '%$DEVICE%' AND job_action = 1 ORDER BY result_id DESC LIMIT 1"`
-if [ -z "$RESULTID" ]
+	IFS="|" read STATUS DEVICENAME BYTES TIMESTART TIMEEND <<< `sqlite3 /volume1/@ActiveBackup/activity.db "select status,device_name,transfered_bytes,time_start,time_end from device_result_table where result_id like '$RESULTID'"`
+if [ -z "$RESULTID" ] 
 then
 	STATUS=0
 	DEVICENAME=$DEVICE
 	BYTES=0
-	TIMESTART=0
-	TIMEEND=0
-else
-	IFS="|" read STATUS DEVICEID DEVICENAME BYTES TIMESTART TIMEEND <<< `sqlite3 /volume1/@ActiveBackup/activity.db "select status,config_device_id,device_name,transfered_bytes,time_start,time_end from device_result_table where result_id like '$RESULTID'"`
 fi
-if [ -z "$TIMEEND" ] || [ $TIMEEND -lt $TIMESTART ]
-	then 
-	TIMEEND=$TIMESTART
+if [ -z "$DEVICENAME" ]
+then
+	STATUS=1
+	DEVICENAME=$DEVICE
+	BYTES=0
 fi
 ACTTIME=`date +%s`
+if [ -z "$TIMEEND" ] || [ $TIMEEND -lt $TIMESTART ]
+	then 
+	TIMESTART=$ACTTIME
+	TIMEEND=$ACTTIME
+fi
 if [ $TIMESTART -eq 0 ]
 then
 	RUNTIME=0
