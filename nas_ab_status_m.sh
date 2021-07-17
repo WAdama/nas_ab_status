5f91d8d180842406372e040bbaa84189e4ca82c2
Ingo <foren@asche-rz.de>
2021-07-17 15:07:56 +0200

    A little optimizing

--- nas_ab_status_m.sh
+++ nas_ab_status_m.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Version 1.0.6
+# Version 1.0.7
 
 CONF=$1
 source $CONF
@@ -9,10 +9,9 @@ echo "<?xml version=\"10.0\" encoding=\"UTF-8\" ?><prtg>"
 for DEVICE in "${DEVICES[@]}"
 do
 IFS="|" read DEVICEID <<< `sqlite3 /volume1/@ActiveBackup/config.db "select device_id from device_table where host_name like '%$DEVICE%'"`
-IFS="|" read TASKDEV <<< `sqlite3 /volume1/@ActiveBackup/config.db "select task_id from backup_task_device where device_id like '$DEVICEID'"`
-IFS="|" read SCHEDULED <<< `sqlite3 /volume1/@ActiveBackup/config.db "select sched_content from task_table where task_id like '$TASKDEV'" | jq .schedule_setting_type`
-
-IFS="|" read RESULTID TASKID <<< `sqlite3 /volume1/@ActiveBackup/activity.db "select result_id,task_id from result_table where task_name like '$TASK' AND task_config like '%$DEVICE%' AND job_action = 1 ORDER BY result_id DESC LIMIT 1"`
+IFS="|" read TASKID <<< `sqlite3 /volume1/@ActiveBackup/config.db "select task_id from backup_task_device where device_id like '$DEVICEID'"`
+IFS="|" read SCHEDULED <<< `sqlite3 /volume1/@ActiveBackup/config.db "select sched_content from task_table where task_id like '$TASKID'" | jq .schedule_setting_type`
+IFS="|" read RESULTID <<< `sqlite3 /volume1/@ActiveBackup/activity.db "select result_id from result_table where task_id like '$TASKID' AND task_config like '%$DEVICE%' AND job_action = 1 ORDER BY result_id DESC LIMIT 1"`
 IFS="|" read STATUS DEVICENAME BYTES TIMESTART TIMEEND <<< `sqlite3 /volume1/@ActiveBackup/activity.db "select status,device_name,transfered_bytes,time_start,time_end from device_result_table where result_id like '$RESULTID'"`
 if [ -z "$RESULTID" ] 
 then
