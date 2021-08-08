a67c492b33f1eb65b84460cbef7cb18eda9b0163
Ingo <foren@asche-rz.de>
2021-08-08 18:50:02 +0200

    Got rid of the oldstyle backticks

--- nas_ab_status_m.sh
+++ nas_ab_status_m.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Version 1.0.7
+# Version 1.0.8
 
 CONF=$1
 source $CONF
@@ -8,11 +8,11 @@ TASK=${TASK^^}
 echo "<?xml version=\"10.0\" encoding=\"UTF-8\" ?><prtg>"
 for DEVICE in "${DEVICES[@]}"
 do
-IFS="|" read DEVICEID <<< `sqlite3 /volume1/@ActiveBackup/config.db "select device_id from device_table where host_name like '%$DEVICE%'"`
-IFS="|" read TASKID <<< `sqlite3 /volume1/@ActiveBackup/config.db "select task_id from backup_task_device where device_id like '$DEVICEID'"`
-IFS="|" read SCHEDULED <<< `sqlite3 /volume1/@ActiveBackup/config.db "select sched_content from task_table where task_id like '$TASKID'" | jq .schedule_setting_type`
-IFS="|" read RESULTID <<< `sqlite3 /volume1/@ActiveBackup/activity.db "select result_id from result_table where task_id like '$TASKID' AND task_config like '%$DEVICE%' AND job_action = 1 ORDER BY result_id DESC LIMIT 1"`
-IFS="|" read STATUS DEVICENAME BYTES TIMESTART TIMEEND <<< `sqlite3 /volume1/@ActiveBackup/activity.db "select status,device_name,transfered_bytes,time_start,time_end from device_result_table where result_id like '$RESULTID'"`
+IFS="|" read DEVICEID <<< $(sqlite3 /volume1/@ActiveBackup/config.db "select device_id from device_table where host_name like '%$DEVICE%'")
+IFS="|" read TASKID <<< $(sqlite3 /volume1/@ActiveBackup/config.db "select task_id from backup_task_device where device_id like '$DEVICEID'")
+IFS="|" read SCHEDULED <<< $(sqlite3 /volume1/@ActiveBackup/config.db "select sched_content from task_table where task_id like '$TASKID'" | jq .schedule_setting_type)
+IFS="|" read RESULTID <<< $(sqlite3 /volume1/@ActiveBackup/activity.db "select result_id from result_table where task_id like '$TASKID' AND task_config like '%$DEVICE%' AND job_action = 1 ORDER BY result_id DESC LIMIT 1")
+IFS="|" read STATUS DEVICENAME BYTES TIMESTART TIMEEND <<< $(sqlite3 /volume1/@ActiveBackup/activity.db "select status,device_name,transfered_bytes,time_start,time_end from device_result_table where result_id like '$RESULTID'")
 if [ -z "$RESULTID" ] 
 then
 	STATUS=0
@@ -25,7 +25,7 @@ then
 	DEVICENAME=$DEVICE
 	BYTES=0
 fi
-ACTTIME=`date +%s`
+ACTTIME=$(date +%s)
 if [ -z "$TIMEEND" ] || [ $TIMEEND -lt $TIMESTART ]
 	then 
 	TIMESTART=$ACTTIME
