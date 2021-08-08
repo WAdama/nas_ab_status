804f305162fab1519844e9a423e2fc8363899490
Ingo <foren@asche-rz.de>
2021-08-08 18:48:07 +0200

    Got rid of the oldstyle backticks

--- nas_ab_status.sh
+++ nas_ab_status.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-# Version 1.0.6
+# Version 1.0.7
 
 DEVICE=${1^^}
 TASK=${2^^}
 
-IFS="|" read DEVICEID <<< `sqlite3 /volume1/@ActiveBackup/config.db "select device_id from device_table where host_name like '%$DEVICE%'"`
-IFS="|" read TASKID <<< `sqlite3 /volume1/@ActiveBackup/config.db "select task_id from backup_task_device where device_id like '$DEVICEID'"`
-IFS="|" read SCHEDULED <<< `sqlite3 /volume1/@ActiveBackup/config.db "select sched_content from task_table where task_id like '$TASKID'" | jq .schedule_setting_type`
-IFS="|" read RESULTID <<< `sqlite3 /volume1/@ActiveBackup/activity.db "select result_id from result_table where task_id like '$TASKID' AND task_config like '%$DEVICE%' AND job_action = 1 ORDER BY result_id DESC LIMIT 1"`
-IFS="|" read STATUS DEVICENAME BYTES TIMESTART TIMEEND <<< `sqlite3 /volume1/@ActiveBackup/activity.db "select status,device_name,transfered_bytes,time_start,time_end from device_result_table where result_id like '$RESULTID'"`
+IFS="|" read DEVICEID <<< $(sqlite3 /volume1/@ActiveBackup/config.db "select device_id from device_table where host_name like '%$DEVICE%'")
+IFS="|" read TASKID <<< $(sqlite3 /volume1/@ActiveBackup/config.db "select task_id from backup_task_device where device_id like '$DEVICEID'")
+IFS="|" read SCHEDULED <<< $(sqlite3 /volume1/@ActiveBackup/config.db "select sched_content from task_table where task_id like '$TASKID'" | jq .schedule_setting_type)
+IFS="|" read RESULTID <<< $(sqlite3 /volume1/@ActiveBackup/activity.db "select result_id from result_table where task_id like '$TASKID' AND task_config like '%$DEVICE%' AND job_action = 1 ORDER BY result_id DESC LIMIT 1")
+IFS="|" read STATUS DEVICENAME BYTES TIMESTART TIMEEND <<< $(sqlite3 /volume1/@ActiveBackup/activity.db "select status,device_name,transfered_bytes,time_start,time_end from device_result_table where result_id like '$RESULTID'")
 if [ -z "$RESULTID" ] 
 then
 	STATUS=0
@@ -21,7 +21,7 @@ then
 	DEVICENAME=$DEVICE
 	BYTES=0
 fi
-ACTTIME=`date +%s`
+ACTTIME=$(date +%s)
 if [ -z "$TIMEEND" ] || [ $TIMEEND -lt $TIMESTART ]
 	then 
 	TIMESTART=$ACTTIME
