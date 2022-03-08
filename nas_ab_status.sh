2eb823ed41e856449befd5f6659881cb84104837
Ingo <foren@asche-rz.de>
2022-03-08 20:49:18 +0100

    Stripped some unnecessary code

--- nas_ab_status.sh
+++ nas_ab_status.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-# Version 1.0.8
+# Version 1.0.9
 
 DEVICE=${1^^}
 TASK=${2^^}
 
-IFS="|" read DEVICEID <<< $(sqlite3 /volume1/@ActiveBackup/config.db "select device_id from device_table where host_name like '%$DEVICE%'")
-IFS="|" read TASKID <<< $(sqlite3 /volume1/@ActiveBackup/config.db "select task_id from backup_task_device where device_id like '$DEVICEID'")
-IFS="|" read SCHEDULED <<< $(sqlite3 /volume1/@ActiveBackup/config.db "select sched_content from task_table where task_id like '$TASKID'" | jq .schedule_setting_type)
-IFS="|" read RESULTID <<< $(sqlite3 /volume1/@ActiveBackup/activity.db "select result_id from result_table where task_id like '$TASKID' AND task_config like '%$DEVICE%' AND job_action = 1 ORDER BY result_id DESC LIMIT 1")
+DEVICEID=$(sqlite3 /volume1/@ActiveBackup/config.db "select device_id from device_table where host_name like '%$DEVICE%'")
+TASKID=$(sqlite3 /volume1/@ActiveBackup/config.db "select task_id from backup_task_device where device_id like '$DEVICEID'")
+SCHEDULED=$(sqlite3 /volume1/@ActiveBackup/config.db "select sched_content from task_table where task_id like '$TASKID'" | jq .schedule_setting_type)
+RESULTID=$(sqlite3 /volume1/@ActiveBackup/activity.db "select result_id from result_table where task_id like '$TASKID' AND task_config like '%$DEVICE%' AND job_action = 1 ORDER BY result_id DESC LIMIT 1")
 IFS="|" read STATUS DEVICENAME BYTES TIMESTART TIMEEND <<< $(sqlite3 /volume1/@ActiveBackup/activity.db "select status,device_name,transfered_bytes,time_start,time_end from device_result_table where result_id like '$RESULTID'")
 if [ -z "$RESULTID" ] 
 then
