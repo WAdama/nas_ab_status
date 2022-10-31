d5b524c528fa5335f1da6b3c643cfe36b15a8fc7
Ingo <foren@asche-rz.de>
2022-10-31 21:32:24 +0100

    Update nas_ab_status.sh

--- nas_ab_status.sh
+++ nas_ab_status.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Version 1.0.9
+# Version 1.0.10
 
 DEVICE=${1^^}
 TASK=${2^^}
@@ -36,5 +36,5 @@ else
 	LASTRUN=$(($ACTTIME - $TIMESTART))
 fi
 echo "<?xml version=\"10.0\" encoding=\"UTF-8\" ?><prtg><result><channel>Status</channel><value>$STATUS</value><ValueLookup>prtg.standardlookups.nas.abstatus</ValueLookup><ShowChart>0</ShowChart></result><result><channel>Job Scheduled</channel><value>$SCHEDULED</value><ValueLookup>prtg.standardlookups.nas.abstatus.scheduled</ValueLookup><ShowChart>0</ShowChart></result><result><channel>Duration</channel><value>$RUNTIME</value><unit>TimeSeconds</unit></result><result><channel>Last Job Run</channel><value>$LASTRUN</value><unit>TimeSeconds</unit><LimitMode>1</LimitMode><LimitMaxWarning>129600</LimitMaxWarning><LimitMaxError>216000</LimitMaxError></result><result><channel>Data transferred</channel><value>$BYTES</value><unit>BytesDisk</unit><VolumeSize>GigaByte</VolumeSize></result>"
-echo "<text>Task: $TASK / Device: $DEVICENAME</text></prtg>"
+echo "<text>Task: $TASK / Device: $DEVICE</text></prtg>"
 exit
